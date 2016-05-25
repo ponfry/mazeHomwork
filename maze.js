7af4dd4ec8d3b408ba235eb8eae9b598792606ca
ponfry <paschaotdel@gmail.com>
2016-05-25 14:52:48 +0300

    Создана функция для заполнения лабиринта

--- maze.js
+++ maze.js
@@ -3,19 +3,47 @@
 });
 
 function builtMaze() {
+    formMaze();
+    amaze(MAZE["currentPosition"][0], MAZE["currentPosition"][1], true);
+    fillMaze();
+}
+
+function formMaze() {
     for (var y = 0; y < SIZE["height"]; y++) {
         MAZE["maze"][y] = [];
         for (var x = 0; x < SIZE["width"]; MAZE["maze"][y][x++] = 'wall') {
             $(".maze").append("<div class=\"block wall\" id=" + y + "-" + x + "></div>");
         }
     }
-    amaze(MAZE["currentPosition"][0], MAZE["currentPosition"][1], true);
-
-
 }
+
+function fillMaze() {
+    while (MAZE["walls"].length != 0) {
+        var randomWall = MAZE["walls"][Math.floor(Math.random() *  MAZE["walls"].length)],
+            host = randomWall[2],
+            opposite = [(host[0] + (randomWall[0] - host[0]) * 2),
+                (host[1] + (randomWall[1] - host[1]) * 2)];
+
+        if (valid(opposite[0], opposite[1])) {
+            if (MAZE["maze"][opposite[0]][opposite[1]] == 'maze') MAZE["walls"].splice( MAZE["walls"].indexOf(randomWall), 1);
+
+            else {
+                amaze(randomWall[0], randomWall[1], false);
+                amaze(opposite[0], opposite[1], true);
+            }
+        }
+        else MAZE["walls"].splice(MAZE["walls"].indexOf(randomWall), 1);
+    }
+
+    $("#0-0").removeClass().addClass("block start");
+    $("#65-135").removeClass().addClass("block finish");
+}
+
+
 function amaze(y, x, addBlockWalls) {
     MAZE["maze"][y][x] = 'maze';
     $("#" + y + "-" + x).removeClass().addClass("block");
+
     if (addBlockWalls && valid(y + 1, x) && (MAZE["maze"][y + 1][x] == 'wall')) MAZE["walls"].push([y + 1, x, [y, x]]);
     if (addBlockWalls && valid(y - 1, x) && (MAZE["maze"][y - 1][x] == 'wall')) MAZE["walls"].push([y - 1, x, [y, x]]);
     if (addBlockWalls && valid(y, x + 1) && (MAZE["maze"][y][x + 1] == 'wall')) MAZE["walls"].push([y, x + 1, [y, x]]);
